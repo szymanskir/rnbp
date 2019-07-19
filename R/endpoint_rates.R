2fa383c157b8f8e8cf994751e60138fe6d119f2a
szymanskir <ryszard.szymanski@outlook.com>
2019-07-19 15:34:10 +0200

    Rates endpoint calls (#4)
    * Add initial implementation of rates endpoint fetching functions
    
    * Add rates endpoint tests

--- R/endpoint_rates.R
+++ R/endpoint_rates.R
@@ -1,19 +1,71 @@
-get_exchangerate <- function(currency_code, table) {
-    
+.rates_base_url <- function() {
+    add_path_part(nbp_api_base_url(), "exchangerates/rates")
 }
 
-get_last_n_exchangerates <- function(currency_code, table, n) {
-    
+.send_rates_endpoint_request <- function(request_url) {
+    response <- send_get_request(url = request_url)
+    parse_rates_endpoint_response(response)
 }
 
-get_todays_exchangerate <- function(currency_code, table) {
-    
+get_exchangerate <- function(table, currency_code) {
+    assert(is.character(table),
+           "The table parameter should be a character.")
+    assert(is.character(currency_code),
+           "The currency_code parameter should be a character.")
+
+    request_url <- create_request(base_url = .rates_base_url(),
+                                  path_parts = c(table, currency_code))
+
+    .send_rates_endpoint_request(request_url)
 }
 
-get_exchangerate_from <- function(currency_code, table, date) {
-    
+get_last_n_exchangerates <- function(table, currency_code, n) {
+    assert(is.character(table),
+           "The table parameter should be a character.")
+    assert(is.character(currency_code),
+           "The currency_code parameter should be a character.")
+
+    request_url <- create_request(base_url = .rates_base_url(),
+                                  path_parts = c(table, currency_code, "last", n))
+
+    .send_rates_endpoint_request(request_url)
 }
 
-get_exchangerate_from_interval <- function(currency_code, table, from, to) {
-    
+get_todays_exchangerate <- function(table, currency_code) {
+    assert(is.character(table),
+           "The table parameter should be a character.")
+    assert(is.character(currency_code),
+           "The currency_code parameter should be a character.")
+
+    request_url <- create_request(base_url = .rates_base_url(),
+                                  path_parts = c(table, currency_code, "today"))
+
+    .send_rates_endpoint_request(request_url)
+}
+
+get_exchangerate_from <- function(table, currency_code, date) {
+    assert(is.character(table),
+           "The table parameter should be a character.")
+    assert(is.character(currency_code),
+           "The currency_code parameter should be a character.")
+
+    request_url <- create_request(base_url = .rates_base_url(),
+                                  path_parts = c(table, currency_code, as.character(date)))
+
+    .send_rates_endpoint_request(request_url)
+}
+
+get_exchangerate_from_interval <- function(table, currency_code, from, to) {
+    assert(is.character(table),
+           "The table parameter should be a character.")
+    assert(is.character(currency_code),
+           "The currency_code parameter should be a character.")
+
+    request_url <- create_request(base_url = .rates_base_url(),
+                                  path_parts = c(table,
+                                                 currency_code,
+                                                 as.character(from),
+                                                 as.character(to)))
+
+    .send_rates_endpoint_request(request_url)
 }
--- R/parser.R
+++ R/parser.R
@@ -2,13 +2,18 @@
     if (httr::http_type(response) != "application/json") {
         stop("The given response is not of type json", call. = FALSE)
     }
-    
+
     content <- httr::content(response, "text")
     jsonlite::fromJSON(content)
 }
 
 parse_tables_endpoint_response <- function(response) {
     json_content <- .parse_json(response)
-    
+    create_nbp_api_response(parsed_content = json_content, url = response$url, response = response)
+}
+
+
+parse_rates_endpoint_response <- function(response) {
+    json_content <- .parse_json(response)
     create_nbp_api_response(parsed_content = json_content, url = response$url, response = response)
 }
